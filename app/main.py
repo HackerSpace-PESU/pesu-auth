94c3085a817e28e2050957185fa5399728cfe15b
aditeyabaral <aditeya.baral@gmail.com>
2021-07-18 01:20:27 +0530

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,32 @@
+import pytz
+import json
+import datetime
+from flask import Flask, request
+from utils import *
+
+
+app = Flask(__name__)
+IST = pytz.timezone('Asia/Kolkata')
+
+
+@app.route("/", methods=["POST"])
+def home():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    login_status = False
+    profile_data = dict()
+    if username != None and password != None:
+        login_status, profile_data = authenticatePESU(username, password)
+
+    result = {
+        "authentication-status": login_status,
+        "timestamp": str(datetime.datetime.now(IST)),
+        "profile-data": profile_data
+    }
+    result = json.dumps(result)
+    return result, 200
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ app/utils.py
@@ -0,0 +1,67 @@
+import os
+import time
+from dotenv import load_dotenv
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument('--ignore-ssl-errors=yes')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--allow-running-insecure-content')
+
+load_dotenv()
+GOOGLE_CHROME_BIN = os.environ["GOOGLE_CHROME_BIN"]
+CHROMEDRIVER_PATH = os.environ["CHROMEDRIVER_PATH"]
+
+
+def authenticatePESU(username, password):
+    chrome = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+    # chrome = webdriver.Chrome(
+    #    executable_path="chromedriver.exe", options=chrome_options)
+    chrome.get("https://pesuacademy.com/Academy")
+    time.sleep(2)
+
+    username_box = chrome.find_element_by_xpath(r'//*[@id="j_scriptusername"]')
+    password_box = chrome.find_element_by_xpath(r'//*[@name="j_password"]')
+
+    username_box.send_keys(username)
+    time.sleep(0.3)
+    password_box.send_keys(password)
+    time.sleep(0.3)
+
+    sign_in_button = chrome.find_element_by_xpath(
+        r'//*[@id="postloginform#/Academy/j_spring_security_check"]')
+    sign_in_button.click()
+    time.sleep(1)
+
+    login_status = None
+    profile_data = dict()
+    try:
+        menu_options = chrome.find_elements_by_xpath(
+            r'//*[@class="menu-name"]')
+        menu_options[9].click()
+        time.sleep(0.3)
+        login_status = True
+    except:
+        login_status = False
+
+    if login_status:
+
+        text_boxes = chrome.find_elements_by_xpath(
+            r'//*[@class="col-md-12 col-xs-12 control-label text-left"]')[:7]
+        text_content = [text_box.text.strip() for text_box in text_boxes]
+        name, prn, srn, degree, branch, semester, section = text_content
+        profile_data["name"] = name
+        profile_data["prn"] = prn
+        profile_data["srn"] = srn
+        profile_data["degree"] = degree
+        profile_data["branch"] = branch
+        profile_data["semester"] = semester
+        profile_data["section"] = section.split()[1]
+
+    chrome.quit()
+    return login_status, profile_data
