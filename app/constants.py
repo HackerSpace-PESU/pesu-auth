8d58c70a46b3db36994c10dc1f49277aef11c01b
aditeyabaral <aditeya.baral@gmail.com>
2025-06-08 11:53:12 -0400

    Minor update to dataclass

--- app/constants.py
+++ app/constants.py
@@ -1,38 +1,32 @@
-from dataclasses import dataclass, field
 from typing import List, Dict
 
 
-@dataclass(frozen=True)
 class PESUAcademyConstants:
-    DEFAULT_FIELDS: List[str] = field(
-        default_factory=lambda: [
-            "name",
-            "prn",
-            "srn",
-            "program",
-            "branch_short_code",
-            "branch",
-            "semester",
-            "section",
-            "email",
-            "phone",
-            "campus_code",
-            "campus",
-            "class",
-            "cycle",
-            "department",
-            "institute_name",
-        ]
-    )
+    DEFAULT_FIELDS: List[str] = [
+        "name",
+        "prn",
+        "srn",
+        "program",
+        "branch_short_code",
+        "branch",
+        "semester",
+        "section",
+        "email",
+        "phone",
+        "campus_code",
+        "campus",
+        "class",
+        "cycle",
+        "department",
+        "institute_name",
+    ]
 
-    BRANCH_SHORT_CODES: Dict[str, str] = field(
-        default_factory=lambda: {
-            "Computer Science and Engineering": "CSE",
-            "Computer Science and Engineering (AI&ML)": "CSE(AI&ML)",
-            "Electronics and Communication Engineering": "ECE",
-            "Mechanical Engineering": "ME",
-            "Electrical and Electronics Engineering": "EEE",
-            "Civil Engineering": "CE",
-            "Biotechnology": "BT",
-        }
-    )
+    BRANCH_SHORT_CODES: Dict[str, str] = {
+        "Computer Science and Engineering": "CSE",
+        "Computer Science and Engineering (AI&ML)": "CSE (AI&ML)",
+        "Electronics and Communication Engineering": "ECE",
+        "Mechanical Engineering": "ME",
+        "Electrical and Electronics Engineering": "EEE",
+        "Civil Engineering": "CE",
+        "Biotechnology": "BT",
+    }
